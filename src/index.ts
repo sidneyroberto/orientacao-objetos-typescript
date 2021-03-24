3a64cff2b32547060552f394af1e3fdef14bd4bc
Sidney Sousa <sidney.sousa@ifms.edu.br>
2021-03-24 13:29:50 -0400

    Adicionando incrementos da semana 2

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,4 @@
 import { ContaBancaria } from './models/ContaBancaria'
 
 const conta1 = new ContaBancaria()
-let saldoAtual: number = conta1.consultar()
-console.log(`Saldo inicial: ${saldoAtual}`)
-let deuCerto: boolean = conta1.depositar(50)
-if (deuCerto) {
-    saldoAtual = conta1.consultar()
-    console.log(`Saldo atual: ${saldoAtual}`)
-} else {
-    console.log('Não foi possível realizar o depósito')
-}
-
-deuCerto = conta1.depositar(120)
-if (deuCerto) {
-    saldoAtual = conta1.consultar()
-    console.log(`Saldo atual: ${saldoAtual}`)
-} else {
-    console.log('Não foi possível realizar o depósito')
-}
-
-deuCerto = conta1.sacar(130)
-if (deuCerto) {
-    saldoAtual = conta1.consultar()
-    console.log(`Saldo atual: ${saldoAtual}`)
-} else {
-    console.log('Não foi possível realizar o saque')
-}
-
-deuCerto = conta1.sacar(100)
-if (deuCerto) {
-    saldoAtual = conta1.consultar()
-    console.log(`Saldo atual: ${saldoAtual}`)
-} else {
-    console.log('Não foi possível realizar o saque')
-}
+console.log(conta1)
--- src/models/ContaBancaria.ts
+++ src/models/ContaBancaria.ts
@@ -1,18 +1,47 @@
 export class ContaBancaria {
 
-    saldo: number
+    private _saldo: number
+    private _numero: string
+    private _agencia: string
 
-    constructor() {
-        this.saldo = 0
+    constructor(numero?: string, agencia?: string) {
+        this._saldo = 0
+        this.numero = this._validarNumero(numero) ? numero : '00000-0'
+        this.agencia = this._validarAgencia(agencia) ? agencia : '0000-0'
+    }
+
+    get numero(): string {
+        return this._numero
+    }
+
+    set numero(valor: string) {
+        if(this._validarNumero(valor)) {
+            this._numero = valor
+        } else {
+            console.log('Formato de número inválido')
+        }
+    }
+
+    get agencia(): string {
+        return this._agencia
+    }
+
+    set agencia(valor: string) {
+        const regex = /^\d{4}-\d{1}$/
+        if(regex.test(valor)) {
+            this._agencia = valor
+        } else {
+            console.log('Formato de agência inválido')
+        }
     }
 
     consultar(): number {
-        return this.saldo
+        return this._saldo
     }
 
     depositar(valor: number): boolean {
         if (valor >= 0) {
-            this.saldo += valor
+            this._saldo += valor
             return true
         }
 
@@ -20,8 +49,28 @@ export class ContaBancaria {
     }
 
     sacar(valor: number): boolean {
-        if (valor >= 0 && this.saldo >= valor) {
-            this.saldo -= valor
+        if (valor >= 0 && this._saldo >= valor) {
+            this._saldo -= valor
+            return true
+        }
+
+        return false
+    }
+
+    private _validarNumero(numero: string): boolean {
+        const regex = /^\d{5}-\d{1}$/
+        if(regex.test(numero)) {
+            this._numero = numero
+            return true
+        } 
+
+        return false
+    }
+
+    private _validarAgencia(agencia: string) : boolean {
+        const regex = /^\d{4}-\d{1}$/
+        if(regex.test(agencia)) {
+            this._agencia = agencia
             return true
         }
 
